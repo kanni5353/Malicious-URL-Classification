46fe28287e715b7e7f1a8ad779f4c5b0856247f0
kanni5353 <145745797+kanni5353@users.noreply.github.com>
2025-05-22 14:03:43 +0530

    Update batch_url_classifier.py

--- src/batch_url_classifier.py
+++ src/batch_url_classifier.py
@@ -2,7 +2,6 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.datasets import load_svmlight_file
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
